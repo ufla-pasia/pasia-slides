15e4c45e273a849b8e58ec47e193d5e45d95789b
Julio César Alves <julio.caburu@gmail.com>
2025-08-27 11:56:57 -0300

    Exercícios

--- codigos/aula_p3.py
+++ codigos/aula_p3.py
@@ -30,3 +30,18 @@ def test_maior_palavra_strings_vazias():
 
 def test_maior_palavra_lista_vazia():
     assert maior_palavra([]) is None
+
+
+# Situação 2
+
+def calcular_quantidade_gols(dados, pais):
+    """
+    dados contém dados estatísticos de futebol:
+    - a terceira coluna traz o nome do país do jogador
+    - a sexta coluna o nome da liga
+    - a nona coluna o número de gols feitos pelo jogador
+    pais é a identificação do país de interesse (ex.: "br BRA")
+
+    A função calcula e retorna a quantidade de gols feitos 
+    por jogadores do país passado em cada liga
+    """
